bf3b97168f04962bcf2d39bb294bdd8338fb78f7
omaribrahimmm <110747019+omaribrahimmm@users.noreply.github.com>
2024-04-21 18:38:11 +0200

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -1,61 +1,43 @@
 #include "headers.h"
 
 
-int msgq_id;
 int main(int argc, char * argv[])
 {
-
     initClk();
-    int process_Count = atoi(argv[1]);
-    int processes_done = 0;
 
-    PCB Processes[process_Count];
+    //Reading file parameters
+    int processes_count = atoi(argv[1]);
+    int algo = atoi(argv[2]);
+    int quantum = atoi(argv[3]);
+    PCB process;
 
-    SchedulingALgo Algo = atoi(argv[2]);
+    //Initiating Message Queue
 
-    
+    key_t key_pg_s = ftok("keyfile", 1);
+    int pg_s_id = msgget(key_pg_s, IPC_CREAT | 0666);
+    if (pg_s_id == -1)
+    {
+        perror("Error in creating pg_s msg queue");
+        return -1;
+    }
+    printf("PGenerator-Scheduler Message Queue ID = %d\n", pg_s_id);
+
+    while(true)
+    {
+        int rec_val = msgrcv(pg_s_id, &process, sizeof(process), 0, !IPC_NOWAIT);
+        if (rec_val == -1)
+        {
+            perror("Error in receiving pg_s");
+            exit(-1);
+        }
+        else
+        {
+            printf("process[%d] arrived at time %d\n", process.id, getClk());
+        }
+    }
+
     //TODO implement the scheduler :)
-    key_t key_id;
-    int rec_val, send_val;
-
-    key_id = ftok("processes.txt", 65);               //create unique key
-    msgq_id = msgget(key_id, 0666 | IPC_CREAT); //create message queue and return id
-
-    if (msgq_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
-    }
-    printf("Message Queue ID = %d\n", msgq_id);
-
-    struct msgbuff message;
-
-    int i = 0;
-    while(1)
-    {
-        rec_val = msgrcv(msgq_id, &message, sizeof(message.Process), 1, !IPC_NOWAIT);
-
-
-            if (rec_val == -1)
-                perror("Error in receive scheduler");
-            else
-                {
-                    printf("\nMessage received: %d %d %d %d %d %d %d %d %d\n", message.Process.id, message.Process.arrival_time, message.Process.running_time, message.Process.priority, message.Process.pid, message.Process.waiting_time,
-                    message.Process.remaining_time,
-                    message.Process.finish_time,
-                    message.Process.state);
-
-                            //Initalize the PCB
-                            Processes[i]= message.Process;
-                }
-        processes_done++;
-    }
-
-
-    //delete the message queue:
-    msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
-    printf("Message Queue terminated\n");
     //upon termination release the clock resources.
-    
+
     destroyClk(true);
 }
